045df65dc3072e77a0d5e9f5cb85cfa4db38bae4
Adam <thomasadam83@hotmail.com>
2023-02-01 10:12:12 -0500

    Added toggle to section links

--- accordion.js
+++ accordion.js
@@ -1,6 +1,14 @@
 
 function hideDisplaySection(section)
 {
-    alert('hello!');
-    alert(section);
+    var target = document.getElementById(section);
+
+    if(target.style.display == "block")
+    {
+        target.style.display = "none";
+    }
+    else
+    {
+        target.style.display = "block";
+    }
 }
