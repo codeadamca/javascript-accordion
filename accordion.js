804a61c1ddc12a04e035f55968e5c27b0e831e0c
Adam Thomas <thomasadam83@hotmail.com>
2024-10-08 07:11:58 -0400

    Update accordion.js

--- accordion.js
+++ accordion.js
@@ -1,14 +1,1 @@
 
-function hideDisplaySection(section)
-{
-    var target = document.getElementById(section);
-
-    if(target.style.display == "block")
-    {
-        target.style.display = "none";
-    }
-    else
-    {
-        target.style.display = "block";
-    }
-}
